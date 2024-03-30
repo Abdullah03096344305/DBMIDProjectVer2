aadc5f84b7ea392162026d2780471ed2ed443e4e
Abdullah Nazir Awan <abdullah03096344305@gmail.com>
2024-03-30 16:48:44 +0500

    Generate report functionality added

--- Forms/FormGenerateReport.Designer.cs
+++ Forms/FormGenerateReport.Designer.cs
@@ -29,94 +29,73 @@
         private void InitializeComponent()
         {
             label7 = new Label();
-            addbtn = new Button();
-            button2 = new Button();
-            button1 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            GenerateButton = new Button();
+            QueryTextBox = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
             SuspendLayout();
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 16F);
+            label7.Font = new Font("Times New Roman", 16F, FontStyle.Bold);
             label7.Location = new Point(231, 9);
             label7.Name = "label7";
-            label7.Size = new Size(182, 30);
+            label7.Size = new Size(177, 25);
             label7.TabIndex = 38;
             label7.Text = "Generate Reports";
-            label7.Click += label7_Click;
             // 
-            // addbtn
+            // GenerateButton
             // 
-            addbtn.BackColor = Color.FromArgb(0, 150, 136);
-            addbtn.Font = new Font("Segoe UI", 11F);
-            addbtn.ForeColor = Color.White;
-            addbtn.Location = new Point(218, 89);
-            addbtn.Name = "addbtn";
-            addbtn.Size = new Size(195, 32);
-            addbtn.TabIndex = 40;
-            addbtn.Text = "Report --> 1";
-            addbtn.UseVisualStyleBackColor = false;
+            GenerateButton.BackColor = Color.FromArgb(0, 150, 136);
+            GenerateButton.Font = new Font("Segoe UI", 11F);
+            GenerateButton.ForeColor = Color.White;
+            GenerateButton.Location = new Point(218, 311);
+            GenerateButton.Name = "GenerateButton";
+            GenerateButton.Size = new Size(195, 32);
+            GenerateButton.TabIndex = 40;
+            GenerateButton.Text = "Generate Report ";
+            GenerateButton.UseVisualStyleBackColor = false;
+            GenerateButton.Click += GenerateButton_Click;
             // 
-            // button2
+            // QueryTextBox
             // 
-            button2.BackColor = Color.FromArgb(0, 150, 136);
-            button2.Font = new Font("Segoe UI", 11F);
-            button2.ForeColor = Color.White;
-            button2.Location = new Point(218, 153);
-            button2.Name = "button2";
-            button2.Size = new Size(195, 32);
-            button2.TabIndex = 42;
-            button2.Text = "Report --> 2";
-            button2.UseVisualStyleBackColor = false;
+            QueryTextBox.BackColor = Color.FromArgb(0, 150, 136);
+            QueryTextBox.Font = new Font("Segoe UI", 11F);
+            QueryTextBox.ForeColor = Color.White;
+            QueryTextBox.Location = new Point(106, 56);
+            QueryTextBox.Multiline = true;
+            QueryTextBox.Name = "QueryTextBox";
+            QueryTextBox.Size = new Size(433, 176);
+            QueryTextBox.TabIndex = 41;
             // 
-            // button1
+            // textBox1
             // 
-            button1.BackColor = Color.FromArgb(0, 150, 136);
-            button1.Font = new Font("Segoe UI", 11F);
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(218, 212);
-            button1.Name = "button1";
-            button1.Size = new Size(195, 32);
-            button1.TabIndex = 43;
-            button1.Text = "Report --> 3";
-            button1.UseVisualStyleBackColor = false;
+            textBox1.Location = new Point(218, 250);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(195, 23);
+            textBox1.TabIndex = 42;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
-            // button3
+            // label1
             // 
-            button3.BackColor = Color.FromArgb(0, 150, 136);
-            button3.Font = new Font("Segoe UI", 11F);
-            button3.ForeColor = Color.White;
-            button3.Location = new Point(218, 274);
-            button3.Name = "button3";
-            button3.Size = new Size(195, 32);
-            button3.TabIndex = 44;
-            button3.Text = "Report --> 4";
-            button3.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            button4.BackColor = Color.FromArgb(0, 150, 136);
-            button4.Font = new Font("Segoe UI", 11F);
-            button4.ForeColor = Color.White;
-            button4.Location = new Point(218, 336);
-            button4.Name = "button4";
-            button4.Size = new Size(195, 32);
-            button4.TabIndex = 45;
-            button4.Text = "Report --> 5";
-            button4.UseVisualStyleBackColor = false;
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 14F, FontStyle.Bold);
+            label1.Location = new Point(80, 251);
+            label1.Name = "label1";
+            label1.Size = new Size(132, 22);
+            label1.TabIndex = 43;
+            label1.Text = "Pdf File Name:";
             // 
             // FormGenerateReport
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(648, 462);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button1);
-            Controls.Add(button2);
-            Controls.Add(addbtn);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(QueryTextBox);
+            Controls.Add(GenerateButton);
             Controls.Add(label7);
             Name = "FormGenerateReport";
             Text = "FormGenerateReport";
@@ -127,10 +106,9 @@
         #endregion
 
         private Label label7;
-        private Button addbtn;
-        private Button button2;
-        private Button button1;
-        private Button button3;
-        private Button button4;
+        private Button GenerateButton;
+        private TextBox QueryTextBox;
+        private TextBox textBox1;
+        private Label label1;
     }
 }
--- Forms/FormGenerateReport.cs
+++ Forms/FormGenerateReport.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ver2.Forms
@@ -17,7 +15,73 @@ namespace ver2.Forms
             InitializeComponent();
         }
 
-        private void label7_Click(object sender, EventArgs e)
+        public class PDFGenerator
+        {
+            public void GeneratePDF(string query, string outputPath)
+            {
+                try
+                {
+                   
+                    string connectionString = "Server=DESKTOP-GSR1O2M;Database=MidProject;Integrated Security=True;";
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+
+                      
+                        SqlCommand command = new SqlCommand(query, connection);
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+
+                        
+                        Document document = new Document();
+                        PdfWriter.GetInstance(document, new FileStream(outputPath, FileMode.Create));
+                        document.Open();
+
+                        PdfPTable pdfTable = new PdfPTable(dataTable.Columns.Count);
+                        foreach (DataColumn column in dataTable.Columns)
+                        {
+                            pdfTable.AddCell(new Phrase(column.ColumnName));
+                        }
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            foreach (var cell in row.ItemArray)
+                            {
+                                pdfTable.AddCell(new Phrase(cell.ToString()));
+                            }
+                        }
+                        document.Add(pdfTable);
+
+                        document.Close();
+                    }
+                    MessageBox.Show("PDF generated successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            string query = QueryTextBox.Text;
+            string fileName = textBox1.Text;
+            string directoryPath = "D:\\UET\\4th_Semester\\DB-LAB\\MID-Project\\"; 
+
+            
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string outputPath = Path.Combine(directoryPath, fileName + ".pdf");
+
+            PDFGenerator pdfGenerator = new PDFGenerator();
+            pdfGenerator.GeneratePDF(query, outputPath);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }

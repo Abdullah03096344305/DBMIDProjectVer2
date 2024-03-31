a27424f5aed8598cb553e90db7e5a4147f43d856
Abdullah Nazir Awan <abdullah03096344305@gmail.com>
2024-04-01 00:09:34 +0500

    FormAssignAdvisor Done

--- Forms/FormAssignAdvisor.Designer.cs
+++ Forms/FormAssignAdvisor.Designer.cs
@@ -30,6 +30,12 @@
         {
             groupgridview = new DataGridView();
             dataGridView1 = new DataGridView();
+            MainAdvisorBox = new ComboBox();
+            CoAdvisorBox = new ComboBox();
+            IndustrialAdvisorBox = new ComboBox();
+            addbtn = new Button();
+            addbtn2 = new Button();
+            addbtn3 = new Button();
             ((System.ComponentModel.ISupportInitialize)groupgridview).BeginInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -37,28 +43,100 @@
             // groupgridview
             // 
             groupgridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            groupgridview.Location = new Point(12, 12);
+            groupgridview.Location = new Point(12, -1);
             groupgridview.Name = "groupgridview";
             groupgridview.Size = new Size(624, 164);
             groupgridview.TabIndex = 42;
+            groupgridview.CellContentClick += groupgridview_CellContentClick;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 272);
+            dataGridView1.Location = new Point(12, 169);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.Size = new Size(624, 178);
             dataGridView1.TabIndex = 43;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // MainAdvisorBox
+            // 
+            MainAdvisorBox.FormattingEnabled = true;
+            MainAdvisorBox.Location = new Point(12, 353);
+            MainAdvisorBox.Name = "MainAdvisorBox";
+            MainAdvisorBox.Size = new Size(198, 23);
+            MainAdvisorBox.TabIndex = 44;
+            // 
+            // CoAdvisorBox
+            // 
+            CoAdvisorBox.FormattingEnabled = true;
+            CoAdvisorBox.Location = new Point(12, 382);
+            CoAdvisorBox.Name = "CoAdvisorBox";
+            CoAdvisorBox.Size = new Size(198, 23);
+            CoAdvisorBox.TabIndex = 45;
+            // 
+            // IndustrialAdvisorBox
+            // 
+            IndustrialAdvisorBox.FormattingEnabled = true;
+            IndustrialAdvisorBox.Location = new Point(12, 411);
+            IndustrialAdvisorBox.Name = "IndustrialAdvisorBox";
+            IndustrialAdvisorBox.Size = new Size(198, 23);
+            IndustrialAdvisorBox.TabIndex = 46;
+            // 
+            // addbtn
+            // 
+            addbtn.BackColor = Color.Navy;
+            addbtn.Font = new Font("Segoe UI", 9F);
+            addbtn.ForeColor = Color.White;
+            addbtn.Location = new Point(232, 353);
+            addbtn.Name = "addbtn";
+            addbtn.Size = new Size(82, 23);
+            addbtn.TabIndex = 47;
+            addbtn.Text = "ADD";
+            addbtn.UseVisualStyleBackColor = false;
+            addbtn.Click += addbtn_Click;
+            // 
+            // addbtn2
+            // 
+            addbtn2.BackColor = Color.Navy;
+            addbtn2.Font = new Font("Segoe UI", 9F);
+            addbtn2.ForeColor = Color.White;
+            addbtn2.Location = new Point(232, 382);
+            addbtn2.Name = "addbtn2";
+            addbtn2.Size = new Size(82, 23);
+            addbtn2.TabIndex = 48;
+            addbtn2.Text = "ADD";
+            addbtn2.UseVisualStyleBackColor = false;
+            addbtn2.Click += addbtn2_Click;
+            // 
+            // addbtn3
+            // 
+            addbtn3.BackColor = Color.Navy;
+            addbtn3.Font = new Font("Segoe UI", 9F);
+            addbtn3.ForeColor = Color.White;
+            addbtn3.Location = new Point(232, 411);
+            addbtn3.Name = "addbtn3";
+            addbtn3.Size = new Size(82, 23);
+            addbtn3.TabIndex = 49;
+            addbtn3.Text = "ADD";
+            addbtn3.UseVisualStyleBackColor = false;
+            addbtn3.Click += addbtn3_Click;
             // 
             // FormAssignAdvisor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(648, 462);
+            Controls.Add(addbtn3);
+            Controls.Add(addbtn2);
+            Controls.Add(addbtn);
+            Controls.Add(IndustrialAdvisorBox);
+            Controls.Add(CoAdvisorBox);
+            Controls.Add(MainAdvisorBox);
             Controls.Add(dataGridView1);
             Controls.Add(groupgridview);
             Name = "FormAssignAdvisor";
             Text = "FormAssignAdvisor";
+            Load += FormAssignAdvisor_Load;
             ((System.ComponentModel.ISupportInitialize)groupgridview).EndInit();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
@@ -68,5 +146,11 @@
 
         private DataGridView groupgridview;
         private DataGridView dataGridView1;
+        private ComboBox MainAdvisorBox;
+        private ComboBox CoAdvisorBox;
+        private ComboBox IndustrialAdvisorBox;
+        private Button addbtn;
+        private Button addbtn2;
+        private Button addbtn3;
     }
 }
--- Forms/FormAssignAdvisor.cs
+++ Forms/FormAssignAdvisor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,210 @@ namespace ver2.Forms
         public FormAssignAdvisor()
         {
             InitializeComponent();
+        }
+        struct Pair
+        {
+            public Pair(string id, string name)
+            {
+                this.id = id;
+                this.name = name;
+            }
+
+            public string id;
+            public string name;
+        }
+
+       
+        int CurrentAdvisorProjectId = 0;
+
+        private void groupgridview_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FormAssignAdvisor_Load(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT PA.ProjectId,MAX(P.Title) AS ProjectTitle,MAX(P.Description) AS Description,MAX(CASE WHEN PA.AdvisorRole = 11 THEN CONCAT(Pers.FirstName, ' ', Pers.LastName) END) AS MainAdvisor,MAX(CASE WHEN PA.AdvisorRole = 12 THEN CONCAT(Pers.FirstName, ' ', Pers.LastName) END) AS CoAdvisor,MAX(CASE WHEN PA.AdvisorRole = 14 THEN CONCAT(Pers.FirstName, ' ', Pers.LastName) END) AS IndustryAdvisor FROM  ProjectAdvisor PA INNER JOIN Advisor A ON PA.AdvisorId = A.Id JOIN   Project P ON P.Id = PA.ProjectId JOIN Person Pers ON Pers.Id = A.Id WHERE P.Title NOT LIKE '-%' AND A.Salary NOT LIKE '-%' GROUP BY PA.ProjectId", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            groupgridview.DataSource = dt;
+
+            var con2 = Configuration.getInstance().getConnection();
+            SqlCommand cmd2 = new SqlCommand("SELECT * FROM Project WHERE Id NOT IN(SELECT ProjectId FROM ProjectAdvisor) AND Project.Title NOT LIKE '-%'", con2);
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            dataGridView1.DataSource = dt2;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string id = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            CurrentAdvisorProjectId = int.Parse(id);
+            getData("SELECT * FROM Project WHERE Id NOT IN (SELECT ProjectId FROM GroupProject) AND Project.Title NOT LIKE '-%'", dataGridView1);
+            getMainAdvisors();
+            MainAdvisorBox.Enabled = true;
+            CoAdvisorBox.Enabled = true;
+            IndustrialAdvisorBox.Enabled = true;
+            addbtn2.Enabled = false;
+            addbtn3.Enabled = false;
+        }
+        List<Pair> MAdvisor = new List<Pair>();
+        List<Pair> CAdvisor = new List<Pair>();
+        List<Pair> IAdvisor = new List<Pair>();
+
+        private void getMainAdvisors()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT Person.FirstName+' '+Person.LastName, Person.Id as ProjectTitle FROM Advisor JOIN Person ON Advisor.Id=Person.Id WHERE Person.id NOT LIKE '-%'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row[0].ToString();
+                MainAdvisorBox.Items.Add(name);
+                MAdvisor.Add(new Pair(row[1].ToString(), name));
+            }
+        }
+
+        private void getCoAdvisor()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT Person.FirstName+' '+Person.LastName, Person.Id as ProjectTitle FROM Advisor JOIN Person ON Advisor.Id=Person.Id WHERE Person.FirstName+' '+Person.LastName NOT IN (@main_advisor) AND Person.id NOT LIKE '-%'", con);
+            cmd.Parameters.AddWithValue("@main_advisor", MainAdvisorBox.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row[0].ToString();
+                CoAdvisorBox.Items.Add(name);
+                CAdvisor.Add(new Pair(row[1].ToString(), name));
+            }
+        }
+
+        private void getIndustrialAdvisor()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT Person.FirstName+' '+Person.LastName, Person.Id as ProjectTitle FROM Advisor JOIN Person ON Advisor.Id=Person.Id WHERE Person.FirstName+' '+Person.LastName NOT IN (@main_advisor, @co_advisor) AND Person.id NOT LIKE '-%'", con);
+            cmd.Parameters.AddWithValue("@main_advisor", MainAdvisorBox.Text);
+            cmd.Parameters.AddWithValue("@co_advisor", CoAdvisorBox.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
+            {
+                string name = row[0].ToString();
+                IndustrialAdvisorBox.Items.Add(name);
+                IAdvisor.Add(new Pair(row[1].ToString(), name));
+            }
+        }
+
+        private void addbtn2_Click(object sender, EventArgs e)
+        {
+            if (CoAdvisorBox.Text == "")
+            {
+                return;
+            }
+            CoAdvisorBox.Enabled = false;
+            addbtn3.Enabled = true;
+            getIndustrialAdvisor();
+        }
+
+        private void addbtn_Click(object sender, EventArgs e)
+        {
+            if (MainAdvisorBox.Text == "")
+            {
+                return;
+            }
+            MainAdvisorBox.Enabled = false;
+            addbtn2.Enabled = true;
+            getCoAdvisor();
+        }
+
+        private void addbtn3_Click(object sender, EventArgs e)
+        {
+            if (IndustrialAdvisorBox.Text == "")
+            {
+                return;
+            }
+            IndustrialAdvisorBox.Enabled = false;
+            var con = Configuration.getInstance().getConnection();
+            SqlTransaction transaction = con.BeginTransaction();
+            Pair MainAdvisor = getSelectedAdvisor(MAdvisor, MainAdvisorBox.Text);
+            Pair CoAdvisor = getSelectedAdvisor(CAdvisor, CoAdvisorBox.Text);
+            Pair IndustrialAdvisor = getSelectedAdvisor(IAdvisor, IndustrialAdvisorBox.Text);
+            try
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO ProjectAdvisor VALUES(@AdvisorId, @ProjectId, @AdvisorRole, @AssignmentDate)", con, transaction);
+                cmd.Parameters.AddWithValue("@AdvisorId", MainAdvisor.id);
+                cmd.Parameters.AddWithValue("@ProjectId", CurrentAdvisorProjectId);
+                cmd.Parameters.AddWithValue("@AdvisorRole", getAdvisorId("Main Advisor"));
+                cmd.Parameters.AddWithValue("@AssignmentDate", DateTime.Now);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("INSERT INTO ProjectAdvisor VALUES(@AdvisorId, @ProjectId, @AdvisorRole, @AssignmentDate)", con, transaction);
+                cmd.Parameters.AddWithValue("@AdvisorId", CoAdvisor.id);
+                cmd.Parameters.AddWithValue("@ProjectId", CurrentAdvisorProjectId);
+                cmd.Parameters.AddWithValue("@AdvisorRole", getAdvisorId("Co Advisor"));
+                cmd.Parameters.AddWithValue("@AssignmentDate", DateTime.Now);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("INSERT INTO ProjectAdvisor VALUES(@AdvisorId, @ProjectId, @AdvisorRole, @AssignmentDate)", con, transaction);
+                cmd.Parameters.AddWithValue("@AdvisorId", IndustrialAdvisor.id);
+                cmd.Parameters.AddWithValue("@ProjectId", CurrentAdvisorProjectId);
+                cmd.Parameters.AddWithValue("@AdvisorRole", getAdvisorId("Industrial Advisor"));
+                cmd.Parameters.AddWithValue("@AssignmentDate", DateTime.Now);
+                cmd.ExecuteNonQuery();
+                transaction.Commit();
+                MainAdvisorBox.Text = CoAdvisorBox.Text = IndustrialAdvisorBox.Text = "";
+                getData("SELECT * FROM Project WHERE Id NOT IN (SELECT ProjectId FROM ProjectAdvisor) AND Project.Title NOT LIKE '-%'", dataGridView1);
+
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void getData(string query, DataGridView grid)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            grid.DataSource = dt;
+        }
+        private int getAdvisorId(string role)
+        {
+            switch (role)
+            {
+                case "Main Advisor":
+                    return 11;
+                case "Co Advisor":
+                    return 12;
+                case "Industrial Advisor":
+                    return 14;
+                default:
+                    return 0;
+            }
+        }
+
+
+        private Pair getSelectedAdvisor(List<Pair> Advisors, string name)
+        {
+            foreach (Pair p in Advisors)
+            {
+                if (p.name == name)
+                {
+                    return p;
+                }
+            }
+            return new Pair("", "");
         }
     }
 }

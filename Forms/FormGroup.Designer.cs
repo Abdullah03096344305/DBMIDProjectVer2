bc61d5faa66c3545e02299e47bde0c8cb2abc984
Abdullah Nazir Awan <abdullah03096344305@gmail.com>
2024-03-31 22:59:38 +0500

    group form added

--- Forms/FormGroup.Designer.cs
+++ Forms/FormGroup.Designer.cs
@@ -29,187 +29,117 @@
         private void InitializeComponent()
         {
             addbtn = new Button();
+            studentgridview = new DataGridView();
+            comboBox1 = new ComboBox();
             label1 = new Label();
-            regnocombobox = new ComboBox();
             label2 = new Label();
-            projectcomboBox = new ComboBox();
-            listBox1 = new ListBox();
-            button1 = new Button();
-            listBox2 = new ListBox();
-            button2 = new Button();
-            button3 = new Button();
-            dateTimePicker1 = new DateTimePicker();
-            label3 = new Label();
-            confirmButton = new Button();
+            comboBox2 = new ComboBox();
+            addbtn2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)studentgridview).BeginInit();
             SuspendLayout();
             // 
             // addbtn
             // 
-            addbtn.BackColor = Color.LightSeaGreen;
-            addbtn.Location = new Point(391, 12);
+            addbtn.BackColor = Color.Navy;
+            addbtn.Font = new Font("Segoe UI", 11F);
+            addbtn.ForeColor = Color.White;
+            addbtn.Location = new Point(338, 36);
             addbtn.Name = "addbtn";
-            addbtn.Size = new Size(94, 23);
-            addbtn.TabIndex = 38;
+            addbtn.Size = new Size(94, 32);
+            addbtn.TabIndex = 1;
             addbtn.Text = "ADD";
             addbtn.UseVisualStyleBackColor = false;
             addbtn.Click += addbtn_Click;
+            // 
+            // studentgridview
+            // 
+            studentgridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            studentgridview.EditMode = DataGridViewEditMode.EditProgrammatically;
+            studentgridview.Location = new Point(12, 156);
+            studentgridview.Name = "studentgridview";
+            studentgridview.Size = new Size(624, 302);
+            studentgridview.TabIndex = 4;
+            studentgridview.CellContentClick += studentgridview_CellContentClick;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(438, 36);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(198, 23);
+            comboBox1.TabIndex = 13;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 11F);
-            label1.Location = new Point(24, 12);
+            label1.Location = new Point(454, 9);
             label1.Name = "label1";
-            label1.Size = new Size(171, 20);
-            label1.TabIndex = 41;
-            label1.Text = "Student Registration No:";
-            // 
-            // regnocombobox
-            // 
-            regnocombobox.FormattingEnabled = true;
-            regnocombobox.Location = new Point(201, 12);
-            regnocombobox.Name = "regnocombobox";
-            regnocombobox.Size = new Size(159, 23);
-            regnocombobox.TabIndex = 40;
-            regnocombobox.SelectedIndexChanged += regnocombobox_SelectedIndexChanged;
+            label1.Size = new Size(139, 20);
+            label1.TabIndex = 14;
+            label1.Text = "Remaining Projects:";
+            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 11F);
-            label2.Location = new Point(21, 167);
+            label2.Location = new Point(23, 9);
             label2.Name = "label2";
-            label2.Size = new Size(64, 20);
-            label2.TabIndex = 43;
-            label2.Text = "Projects:";
+            label2.Size = new Size(141, 20);
+            label2.TabIndex = 16;
+            label2.Text = "Remaining Students";
             // 
-            // projectcomboBox
+            // comboBox2
             // 
-            projectcomboBox.FormattingEnabled = true;
-            projectcomboBox.Location = new Point(201, 164);
-            projectcomboBox.Name = "projectcomboBox";
-            projectcomboBox.Size = new Size(159, 23);
-            projectcomboBox.TabIndex = 42;
-            projectcomboBox.SelectedIndexChanged += projectcomboBox_SelectedIndexChanged;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(7, 36);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(198, 23);
+            comboBox2.TabIndex = 15;
             // 
-            // listBox1
+            // addbtn2
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(516, 12);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(120, 109);
-            listBox1.TabIndex = 44;
-            // 
-            // button1
-            // 
-            button1.BackColor = Color.LightSeaGreen;
-            button1.Location = new Point(391, 41);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 23);
-            button1.TabIndex = 45;
-            button1.Text = "Delete";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // listBox2
-            // 
-            listBox2.FormattingEnabled = true;
-            listBox2.ItemHeight = 15;
-            listBox2.Location = new Point(516, 164);
-            listBox2.Name = "listBox2";
-            listBox2.Size = new Size(120, 109);
-            listBox2.TabIndex = 46;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.LightSeaGreen;
-            button2.Location = new Point(391, 163);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 23);
-            button2.TabIndex = 47;
-            button2.Text = "ADD";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
-            // 
-            // button3
-            // 
-            button3.BackColor = Color.LightSeaGreen;
-            button3.Location = new Point(391, 192);
-            button3.Name = "button3";
-            button3.Size = new Size(94, 23);
-            button3.TabIndex = 48;
-            button3.Text = "Delete";
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
-            // 
-            // dateTimePicker1
-            // 
-            dateTimePicker1.Location = new Point(201, 273);
-            dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(200, 23);
-            dateTimePicker1.TabIndex = 49;
-            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 11F);
-            label3.Location = new Point(21, 276);
-            label3.Name = "label3";
-            label3.Size = new Size(162, 20);
-            label3.TabIndex = 50;
-            label3.Text = "Project Assigning Date:";
-            // 
-            // confirmButton
-            // 
-            confirmButton.BackColor = Color.LightSeaGreen;
-            confirmButton.Location = new Point(239, 350);
-            confirmButton.Name = "confirmButton";
-            confirmButton.Size = new Size(195, 29);
-            confirmButton.TabIndex = 51;
-            confirmButton.Text = "Confirm";
-            confirmButton.UseVisualStyleBackColor = false;
-            confirmButton.Click += confirmButton_Click;
+            addbtn2.BackColor = Color.Navy;
+            addbtn2.Font = new Font("Segoe UI", 11F);
+            addbtn2.ForeColor = Color.White;
+            addbtn2.Location = new Point(211, 36);
+            addbtn2.Name = "addbtn2";
+            addbtn2.Size = new Size(94, 32);
+            addbtn2.TabIndex = 17;
+            addbtn2.Text = "ADD";
+            addbtn2.UseVisualStyleBackColor = false;
+            addbtn2.Click += addbtn2_Click;
             // 
             // FormGroup
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(648, 462);
-            Controls.Add(confirmButton);
-            Controls.Add(label3);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(listBox2);
-            Controls.Add(button1);
-            Controls.Add(listBox1);
+            Controls.Add(addbtn2);
             Controls.Add(label2);
-            Controls.Add(projectcomboBox);
+            Controls.Add(comboBox2);
             Controls.Add(label1);
-            Controls.Add(regnocombobox);
+            Controls.Add(comboBox1);
+            Controls.Add(studentgridview);
             Controls.Add(addbtn);
             Name = "FormGroup";
             Text = "FormGroup";
             Load += FormGroup_Load;
+            ((System.ComponentModel.ISupportInitialize)studentgridview).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
+
         private Button addbtn;
+        private DataGridView studentgridview;
+        private ComboBox comboBox1;
         private Label label1;
-        private ComboBox regnocombobox;
         private Label label2;
-        private ComboBox projectcomboBox;
-        private ListBox listBox1;
-        private Button button1;
-        private ListBox listBox2;
-        private Button button2;
-        private Button button3;
-        private DateTimePicker dateTimePicker1;
-        private Label label3;
-        private Button confirmButton;
+        private ComboBox comboBox2;
+        private Button addbtn2;
     }
 }
--- Forms/FormGroup.cs
+++ Forms/FormGroup.cs
@@ -2,303 +2,153 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ver2.Forms
 {
     public partial class FormGroup : Form
     {
-        private string connectionString = "Data Source=DESKTOP-GSR1O2M;Initial Catalog=ProjectA;Integrated Security=True";
         public FormGroup()
         {
             InitializeComponent();
         }
+        struct Pair
+        {
+            public Pair(string id, string name)
+            {
+                this.id = id;
+                this.name = name;
+            }
+
+            public string id;
+            public string name;
+        }
+
+        int Editid = 0;
+        int EditAdvisorid = 0;
+        int EditProjectid = 0;
+        int CurrentId = 0;
+
 
         private void FormGroup_Load(object sender, EventArgs e)
         {
             var con = Configuration.getInstance().getConnection();
-            SqlCommand cmd = new SqlCommand("Select RegistrationNo FROM Student", con);
+            SqlCommand cmd = new SqlCommand("Select [Group].Id,[Group].Created_On,Project.Title as ProjectTitle  FROM ([Group] JOIN GroupProject ON [Group].Id=GroupProject.GroupId) JOIN Project ON GroupProject.ProjectId=Project.Id WHERE Project.Title NOT LIKE '-%'", con);
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             da.Fill(dt);
-            LoadRegistrationNumbers();
-            LoadProjects();
-        }
-        private void LoadRegistrationNumbers()
-        {
-            try
-            {
-                string query = "SELECT RegistrationNo FROM Student";
+            studentgridview.DataSource = dt;
+            comboBox1.Items.Clear();
+            getProjectIds();
 
-                // Create connection and command objects
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(query, connection);
-
-                    // Open connection
-                    connection.Open();
-
-                    // Execute command and load results into a DataTable
-                    DataTable table = new DataTable();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        table.Load(reader);
-                    }
-
-                    // Bind the DataTable to the regnocombobox
-                    regnocombobox.DataSource = table;
-                    regnocombobox.DisplayMember = "RegistrationNo";
-                    regnocombobox.ValueMember = "RegistrationNo";
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while loading registration numbers: " + ex.Message);
-            }
         }
 
-
-        private void regnocombobox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-        private void LoadProjects()
-        {
-            try
-            {
-                string query = "SELECT Title FROM Project";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(query, connection);
-                    connection.Open();
-                    DataTable table = new DataTable();
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        table.Load(reader);
-                    }
-                    projectcomboBox.DataSource = table;
-                    projectcomboBox.DisplayMember = "Title";
-                    projectcomboBox.ValueMember = "Title";
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while loading registration numbers: " + ex.Message);
-            }
-        }
-
-        private void projectcomboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
         private void addbtn_Click(object sender, EventArgs e)
         {
-            if (regnocombobox.SelectedIndex > -1)
-            {
-                string selectedItemText = regnocombobox.GetItemText(regnocombobox.SelectedItem);
-                listBox1.Items.Add(selectedItemText);
-            }
-            if (listBox1.SelectedIndex > -1)
-            {
-                string selectedRegNo = listBox1.SelectedItem.ToString();
-                int studentId = GetStudentIdByRegistrationNo(selectedRegNo);
-                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
-
-                // Now you have the studentId, you can use it to insert into the GroupStudent table
-                // along with the selected project and other details.
-            }
-        }
-
-        private void groupgridview_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void studentgridview_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (listBox1.SelectedIndex > -1)
+            int index = studentgridview.CurrentCell.RowIndex;
+            int id = Convert.ToInt32(studentgridview.Rows[index].Cells[0].Value);
+            CurrentId = id;
+            getStudents();
+            getStudentData();
+        }
+        List<Pair> Students = new List<Pair>();
+        List<Pair> Projects = new List<Pair>();
+        private void getProjectIds()
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT id, Title FROM Project WHERE id NOT IN (SELECT Projectid FROM GroupProject) AND Project.Title NOT LIKE '-%'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
             {
-                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                string id = row[0].ToString();
+                comboBox1.Items.Add(row[1].ToString());
+                Projects.Add(new Pair(id, row[1].ToString()));
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void getStudents()
         {
-            if (projectcomboBox.SelectedIndex > -1)
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT Person.id, FirstName + ' ' + LastName FROM Person JOIN Student On Person.id = Student.id WHERE Student.id NOT IN (SELECT Studentid FROM GroupStudent) AND Person.FirstName NOT LIKE '-%'", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            foreach (DataRow row in dt.Rows)
             {
-                string selectedItemText = projectcomboBox.GetItemText(projectcomboBox.SelectedItem);
-                listBox2.Items.Add(selectedItemText);
-            }
-            if (listBox2.SelectedIndex > -1)
-            {
-                string selectedProjectTitle = listBox2.SelectedItem.ToString();
-                int projectId = GetProjectIdByTitle(selectedProjectTitle);
-                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
-
-                // Now you have the projectId, you can use it to insert into the GroupStudent table
-                // along with the selected student and other details.
+                string id = row[0].ToString();
+                comboBox2.Items.Add(row[1].ToString());
+                Students.Add(new Pair(id, row[1].ToString()));
             }
         }
+        private bool isGroupFree()
+        {           
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM GroupStudent JOIN Student ON student.Id = GroupStudent.StudentId WHERE GroupId = @GroupId AND Student.RegistrationNo NOT LIKE '-%'", con);
+            cmd.Parameters.AddWithValue("@GroupId", CurrentId);
+            return (int)cmd.ExecuteScalar() < 5;
+        }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void getStudentData()
         {
-            if (listBox2.SelectedIndex > -1)
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT GroupStudent.GroupId, Student.RegistrationNo, Person.FirstName+' '+Person.LastName as StudentName, CASE WHEN Status = '3' THEN 'ACTIVE' WHEN Status = '4' THEN 'INACTIVE' END AS Status  FROM GroupStudent JOIN Person ON Person.Id=GroupStudent.StudentId JOIN Student ON GroupStudent.StudentId=Student.Id  Where GroupStudent.GroupId=@GroupId AND Student.RegistrationNo NOT LIKE '-%' AND GroupStudent.Status <> 4", con);
+            cmd.Parameters.AddWithValue("@groupId", CurrentId);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            studentgridview.DataSource = dt;
+        }
+
+        private void addbtn2_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text == "")
             {
-                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+                MessageBox.Show("Select a Student to assign");
+                return;
             }
-        }
-        DateTime DoB;
-        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
-        {
-            DoB = DateTime.Parse(dateTimePicker1.Text);
-            MessageBox.Show(Text = DoB.ToString("yyyy-MM-dd"));
-        }
-
-        private int GetStudentIdByRegistrationNo(string registrationNo)
-        {
-            int studentId = -1;
-            try
+            if (!isGroupFree())
             {
-                string query = "SELECT Id FROM Student WHERE RegistrationNo = @RegistrationNo";
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                MessageBox.Show("Group is full");
+                return;
+            }
+            DateTime date = DateTime.Now;
+            var con = Configuration.getInstance().getConnection();
+
+            foreach (Pair p in Students)
+            {
+                if (comboBox2.Text == p.name)
                 {
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@RegistrationNo", registrationNo);
-                    connection.Open();
-                    studentId = (int)command.ExecuteScalar();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO GroupStudent VALUES(@GroupId, @StudentId, @Status, @AssignmentDate)", con);
+                    cmd.Parameters.AddWithValue("@GroupId", CurrentId);
+                    cmd.Parameters.AddWithValue("@StudentId", p.id);
+                    cmd.Parameters.AddWithValue("@Status", 3);
+                    cmd.Parameters.AddWithValue("@AssignmentDate", DateTime.Now);
+                    cmd.ExecuteNonQuery();
+                    break;
                 }
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while fetching student ID: " + ex.Message);
-            }
-            return studentId;
+            MessageBox.Show("Succussfully added");
+            getStudentData();
+            comboBox2.Items.Clear();
+            getStudents();
         }
-
-        private int GetProjectIdByTitle(string title)
-        {
-            int projectId = -1;
-            try
-            {
-                string query = "SELECT Id FROM Project WHERE Title = @Title";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@Title", title);
-                    connection.Open();
-                    projectId = (int)command.ExecuteScalar();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while fetching project ID: " + ex.Message);
-            }
-            return projectId;
-        }
-
-        private void confirmButton_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Insert into the Group table to generate a new Group ID
-                int groupId = InsertGroup();
-
-                // Get selected registration numbers from listBox1
-                List<string> selectedRegistrationNumbers = new List<string>();
-                foreach (var item in listBox1.Items)
-                {
-                    selectedRegistrationNumbers.Add(item.ToString());
-                }
-
-                // Insert each selected registration number into the GroupStudent table
-                foreach (var registrationNumber in selectedRegistrationNumbers)
-                {
-                    // Retrieve Student ID based on Registration Number
-                    int studentId = GetStudentIdByRegistrationNumber(registrationNumber);
-
-                    // Insert into GroupStudent table
-                    InsertGroupStudent(groupId, studentId, DateTime.Now, 1); // Assuming Status = 3 for Active
-                }
-
-                MessageBox.Show("Data inserted successfully!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred: " + ex.Message);
-            }
-        }
-        private int InsertGroup()
-        {
-            int groupId = -1;
-            try
-            {
-                string groupQuery = "INSERT INTO [Group] (Created_On) VALUES (@CreatedOn); SELECT SCOPE_IDENTITY();";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand groupCommand = new SqlCommand(groupQuery, connection);
-                    groupCommand.Parameters.AddWithValue("@CreatedOn", DateTime.Now);
-                    connection.Open();
-                    groupId = Convert.ToInt32(groupCommand.ExecuteScalar()); // Retrieve the generated Group ID
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("An error occurred while inserting data into Group table: " + ex.Message);
-            }
-            return groupId;
-        }
-
-        private int GetStudentIdByRegistrationNumber(string registrationNumber)
-        {
-            int studentId = -1;
-            try
-            {
-                string query = "SELECT Id FROM Student WHERE RegistrationNo = @RegistrationNo";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@RegistrationNo", registrationNumber);
-                    connection.Open();
-                    studentId = Convert.ToInt32(command.ExecuteScalar());
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("An error occurred while retrieving student ID: " + ex.Message);
-            }
-            return studentId;
-        }
-
-        private void InsertGroupStudent(int groupId, int studentId, DateTime assignmentDate, int status)
-        {
-            try
-            {
-                string query = "INSERT INTO GroupStudent (GroupId, StudentId, AssignmentDate, Status) " +
-                               "VALUES (@GroupId, @StudentId, @AssignmentDate, @Status)";
-                using (SqlConnection connection = new SqlConnection(connectionString))
-                {
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@GroupId", groupId);
-                    command.Parameters.AddWithValue("@StudentId", studentId);
-                    command.Parameters.AddWithValue("@AssignmentDate", assignmentDate);
-                    command.Parameters.AddWithValue("@Status", status);
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("An error occurred while inserting data into GroupStudent table: " + ex.Message);
-            }
-        }
-       
-
-
     }
 }
